2af06f5f928fbd728e9682b90631c2c075e73835
wiiznokes <78230769+wiiznokes@users.noreply.github.com>
2024-10-25 02:55:20 +0200

    Update lib.rs

--- cosmic-panel-config/src/lib.rs
+++ cosmic-panel-config/src/lib.rs
@@ -10,13 +10,21 @@ pub use panel_config::*;
 #[cfg(feature = "schema")]
 #[test]
 fn gen_schema() {
-    let appid = "com.system76.CosmicPanel";
+    fn gen(appid: &str, version: i32) {
+        let string = configurator_schema::gen_schema::<CosmicPanelConfig>()
+            .format(configurator_schema::ConfigFormat::CosmicRon)
+            .source_home_path(&format!(".config/cosmic/{}/v{}", appid, version))
+            .source_paths(&[&format!("/usr/share/cosmic/{}/v{}", appid, version)])
+            .call()
+            .unwrap();
+        std::fs::write(&format!("../../schemas/{}.json", appid), &string).unwrap();
+    }
+
+    let appid = "com.system76.CosmicPanel.Dock";
     let version = 1;
-    let string = configurator_schema::gen_schema::<CosmicPanelContainerConfig>()
-        .format(configurator_schema::ConfigFormat::CosmicRon)
-        .source_home_path(&format!(".config/cosmic/{}/v{}", appid, version))
-        .source_paths(&[&format!("/usr/share/cosmic/{}/v{}", appid, version)])
-        .call()
-        .unwrap();
-    std::fs::write(&format!("../../schemas/{}.json", appid), &string).unwrap();
+
+    gen(appid, version);
+    let appid = "com.system76.CosmicPanel.Panel";
+
+    gen(appid, version);
 }
